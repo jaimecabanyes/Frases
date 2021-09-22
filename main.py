e707ec767432aa957d48303acde122031886a0ad
jaimecabanyes <jaimecabanyes@gmail.com>
2021-09-22 17:16:47 +0200

    done

--- main.py
+++ main.py
@@ -39,6 +39,29 @@ def A_C():
     return f.A_C()
 
 
+@app.route("/nuevo_campo", methods=["POST"])
+def nuevo_campo():
+    Campo = request.form.get("Campo")
+    
+    return f.insertar_Campo(Campo)
+
+@app.route("/datos_autor", methods=["POST"])
+def meter_datos():
+    nombre = request.form.get("nombre")
+    año_nacimiento =  request.form.get("año_nacimiento ")
+    Campo = request.form.get("Campo")
+    return f.insertar_datos_autor1(nombre, año_nacimiento, Campo )
+
+@app.route("/insertar_frase", methods=["POST"])
+def meter_frase():
+    nombre = request.form.get("nombre")
+    año_nacimiento =  request.form.get("año_nacimiento ")
+    Campo = request.form.get("Campo")
+    Frase = request.form.get("Frase")
+    return f.insertar_frase(nombre, año_nacimiento, Campo, Frase )
+
+
+
 
 
 
